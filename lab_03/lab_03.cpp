a3d651d42ec31ac951020f3ef433c96e86728fd0
r1dzesu <164938837+r1dzesu@users.noreply.github.com>
2024-03-25 23:17:20 +0200

    Update lab_03.cpp

--- lab_03/lab_03.cpp
+++ lab_03/lab_03.cpp
@@ -14,7 +14,7 @@ int main()
     geometry::circle::input_circle(xc, yc, r);
     geometry::quadr::input_quadr(x1, y1, x2, y2, x3, y3, x4, y4);
 
-    geometry::position_quard_about_circle(xc, yc, r, x1, y2, x2, y2, x3, y3, x4, y4);
+    geometry::position_quard_about_circle(xc, yc, r, x1, y1, x2, y2, x3, y3, x4, y4);
 
     output::bye();
     return 0;
